42d21f669710e2f7c79ab8321815d31df93427c1
Moldokmatovnurbol <portui0000@gmail.com>
2024-12-12 16:48:56 +0600

    Add files via upload

---
+++ homework3/main.js
@@ -0,0 +1,43 @@
+
+
+// function findMin(a, b) {
+//     console.log(Math.min(a, b));
+// }
+// findMin(5, 3);
+// findMin(4, 8);
+// findMin(-10, 5);
+
+
+
+
+// function getStringLength(input) {
+//     console.log(input.length);
+// }
+//
+// let userInput = prompt("Введите строку:");
+// getStringLength(userInput);
+
+
+// function calculator(a, b, operation) {
+//     switch (operation) {
+//         case 'add':
+//             console.log(a + b);
+//             break;
+//         case 'subtract':
+//             console.log(a - b);
+//             break;
+//         case 'multiply':
+//             console.log(a * b);
+//             break;
+//         case 'divide':
+//             if (b !== 0) {
+//                 console.log(a / b);
+//             } else {
+//                 console.log('Ошибка: деление на ноль');
+//             }
+//             break;
+//         default:
+//             console.log('Неизвестная операция');
+//     }
+// }
+
